400756b135f51fff4df97d40cf8699eeb42e52c9
Ryan Crosby <ryan.crosby@live.com>
2015-06-02 05:30:56 +1000

    Further refactoring

--- DirectDraw_Compatibility_Changer/CompatibilityInformation.cs
+++ DirectDraw_Compatibility_Changer/CompatibilityInformation.cs
@@ -7,36 +7,9 @@ using Microsoft.Win32;
 
 namespace DirectDraw_Colourfix {
     public class CompatibilityInformation {
-        public string KeyName;
-        public string Name;
-        public string ID;
-        public string Flags;
-
-        public CompatibilityInformation(string Keyname, bool fromWowNode) {
-            this.KeyName = Keyname;
-            RegistryKey Reg = Registry.LocalMachine;
-            if (fromWowNode) {
-                Reg = Reg.OpenSubKey(MainForm.RegDDPathWow + @"Compatibility\" + KeyName);
-            }
-            else {
-                Reg = Reg.OpenSubKey(MainForm.RegDDPath + @"Compatibility\" + KeyName);
-            }
-
-            Name = (string)Reg.GetValue("Name");
-
-            if (Reg.GetValueKind("ID") == RegistryValueKind.Binary) {
-                byte[] IDBytes = (byte[])Reg.GetValue("ID");
-                ID = Utilities.ByteArrayToHex(IDBytes);
-            }
-            else if (Reg.GetValueKind("ID") == RegistryValueKind.DWord) {
-                byte[] IDBytes = BitConverter.GetBytes((int)Reg.GetValue("ID"));
-                ID = Utilities.ByteArrayToHex(IDBytes);
-            }
-
-            byte[] FlagBytes = (byte[])Reg.GetValue("Flags");
-            Flags = Utilities.ByteArrayToHex(FlagBytes);
-        }
-
-        
+        public string KeyName { get; set; }
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Flags { get; set; }
     }
 }
